6adbd1e31856888c8d349fc0a3e654f3e952f2b0
PredaWnia <pablogscraft@gmail.com>
2022-02-12 23:56:18 +0100

    añadida prueba contadores

--- Software/Js/iniciar.js
+++ Software/Js/iniciar.js
@@ -1,12 +1,20 @@
+var id;
+var cont = 0.0000;
+
 function cambiarIcono() {
-if ($("#boton-datos").attr("src") != "img/pause.png") {
-   $("#boton-datos").attr("src", "img/pause.png");
-   act = false;
-}
-else {
-   $("#boton-datos").attr("src", "img/play.png");
-   act = true;
-}
+   
+   if ($("#boton-datos").attr("src") != "img/pause.png") {
+      $("#boton-datos").attr("src", "img/pause.png");
+      act = false;
+      
+      id = setInterval('contador()', 25);
+   }
+   else {
+      $("#boton-datos").attr("src", "img/play.png");
+      act = true;
+
+      clearInterval(id);
+   }
 }
 
 $(document).ready(function(){
@@ -16,3 +24,15 @@ $(document).ready(function(){
       
    })
 });
+
+function contador(){
+   var contador = document.getElementById("texto-temp");
+   cont = cont.toFixed(4);
+
+	contador.textContent = cont;
+
+	cont = parseFloat(cont) + 0.0001 ;
+}
+
+
+
