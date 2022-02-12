39bbbb0c6230f21faf084a1876914bcc314dd2f0
PredaWnia <pablogscraft@gmail.com>
2022-02-13 00:05:50 +0100

    .

--- Software/Js/iniciar.js
+++ Software/Js/iniciar.js
@@ -1,29 +1,33 @@
 var id;
 var cont = 0.0000;
 
-function cambiarIcono() {
+$(document).ready(function(){
+   $("#boton-datos").click(function () {  
+      
+      toggleRead();
+      
+   })
+});
+
+function toggleRead() {
    
    if ($("#boton-datos").attr("src") != "img/pause.png") {
       $("#boton-datos").attr("src", "img/pause.png");
       act = false;
-      
+
+
       id = setInterval('contador()', 25);
    }
    else {
       $("#boton-datos").attr("src", "img/play.png");
       act = true;
 
+
       clearInterval(id);
    }
 }
 
-$(document).ready(function(){
-   $("#boton-datos").click(function () {  
-      
-      cambiarIcono();
-      
-   })
-});
+
 
 function contador(){
    cont = cont.toFixed(4);
