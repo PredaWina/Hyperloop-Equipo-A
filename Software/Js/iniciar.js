445f471232c6c49343d88feb18df5bcb6f7922fd
PredaWnia <pablogscraft@gmail.com>
2022-02-12 01:40:07 +0100

    algunos arreglos

--- Software/Js/iniciar.js
+++ Software/Js/iniciar.js
@@ -1,14 +1,18 @@
-let aux = "";
+function cambiarIcono() {
+if ($("#boton-datos").attr("src") != "img/pause.png") {
+   $("#boton-datos").attr("src", "img/pause.png");
+   act = false;
+}
+else {
+   $("#boton-datos").attr("src", "img/play.png");
+   act = true;
+}
+}
+
 $(document).ready(function(){
-    $("#boton-datos").click(function () {  
-       if ($("#boton-datos").attr("src") != "img/pause.png") {
-        $("#boton-datos").attr("src", "img/pause.png");
-       } 
-       else{
-        $("#boton-datos").attr("src", "img/play.png");
-       }
-        
-    })
-  });
-
-
+   $("#boton-datos").click(function () {  
+      
+      cambiarIcono();
+      
+   })
+});
