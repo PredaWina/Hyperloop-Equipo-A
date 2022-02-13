30ad04e9eb39217b2af62ab767be5d18c7559179
PredaWnia <pablogscraft@gmail.com>
2022-02-13 04:34:22 +0100

    index finalizado

---
+++ Software/Js/redirigir.js
@@ -0,0 +1,13 @@
+
+
+$(function(){
+    $("#sensores").on("click",function(){
+        window.location = "sensores.html";
+    });
+})
+
+$(function(){
+    $("#hardware").on("click",function(){
+        window.location = "hardware.html";
+    });
+})
