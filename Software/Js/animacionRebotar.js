8cf46b2824a4e27fda7b01dc8d3dd3764dc5dc22
PredaWnia <pablogscraft@gmail.com>
2022-02-13 05:42:37 +0100

    no lo consigo arreglar

--- Software/Js/animacionRebotar.js
+++ Software/Js/animacionRebotar.js
@@ -1,9 +1,10 @@
 var id;
 var cont = 0;
 var maxIteraciones = 15;
+var tiempoEspera = 5000;
 
 function animar() {
-    
+
     $("#sensores").animate({
         top: "-5px"
     }, "fast");
@@ -16,14 +17,22 @@ function animar() {
     $("#sensores").animate({
         top: "0px"
     }, "fast");
+
+    $('#sensores').css({
+        position: 'relative'
+    });
+
+    $("#sensores").stop();
+
+
+    clearInterval(id);
 
     cont++;
     if (cont > maxIteraciones - 1) {
         clearInterval(id);
     }
-    
 }
 
 id = setInterval(function () {
     animar()
-}, 15000);
+}, tiempoEspera);
