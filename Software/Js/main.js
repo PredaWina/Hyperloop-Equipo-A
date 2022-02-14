a78af2b94f773a408cfbba5cb71314520c5ba019
Pablo0304 <gonca.pablo@gmail.com>
2022-02-14 18:49:06 +0100

    MaxMin

--- Software/Js/main.js
+++ Software/Js/main.js
@@ -4,13 +4,16 @@ $(document).ready(function () {
     
 });
 
+var res2 = 0;
+var maximo = 0;
+var minimo = 0;
 var cont = 0.0; 
 const enteros = 2;
 const decimales = 2;
 var read;
 var url = "?";
 var updateRate = 10; // en milisegundos
-var res = 2;
+var res = -2;
 
 function main(){
     id = setInterval("update()", updateRate);
@@ -24,6 +27,8 @@ function update(){
 
         console.log(res);
         mostrarDatos(res);
+        minimoR(res2);
+        maximoR(res2);
         
     }
 }
@@ -35,7 +40,26 @@ function obtenerTemp(res){
 
 // Funcion solo para provar
 function mostrarDatos(datos) {
+    res2 = datos;
     datos = datos.toFixed(decimales);
     
     $("#texto-temp").html(String(datos).padStart(enteros + decimales + 1, "0"));
+}
+
+//Función para poner el mínimo
+function minimoR(datos){
+    if (minimo > res2) {
+        datos = datos.toFixed(decimales);
+        $("#texto-temp2").html(String(datos).padStart(enteros + decimales + 1, "0"));
+        minimo = res2;
+    }
+}
+
+//Función para poner el máximo
+function maximoR(datos){
+    if (maximo < res2) {
+        datos = datos.toFixed(decimales);
+        $("#texto-temp3").html(String(datos).padStart(enteros + decimales + 1, "0"));
+        maximo = res2;
+    }
 }
