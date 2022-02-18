9dd1195d6ea067223ced681bef34c5370140b9f3
Pablo0304 <gonca.pablo@gmail.com>
2022-02-18 13:02:01 +0100

    ManelPablo

--- Software/Js/redirigir.js
+++ Software/Js/redirigir.js
@@ -11,3 +11,24 @@ $(function(){
         window.location = "nosotros.html";
     });
 })
+
+$(function(){
+    $("#software").on("click",function(){
+        
+        window.location = "software.html";
+    });
+})
+
+$(function(){
+    $("#firmware").on("click",function(){
+        
+        window.location = "firmware.html";
+    });
+})
+
+$(function(){
+    $("#hardware").on("click",function(){
+        
+        window.location = "hardware.html";
+    });
+})
